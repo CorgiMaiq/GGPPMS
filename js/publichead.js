f74320ec97ec227743a466747ac4db570681dbaf
CorgiMaiq <a905512150@qq.com>
2019-05-14 22:27:42 +0800

    add some
    someth

--- js/publichead.js
+++ js/publichead.js
@@ -16,6 +16,19 @@ function delCookie(name) {
 	if(cval != null) document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
 }
 
+function mywork(){
+	$("#mywork").click(function(){
+		location.href = "myworkspace.html?username=" + getCookie("username");
+	});
+}
+
+function getUrlParam(name) {
+			var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+			var r = window.location.search.substr(1).match(reg); //匹配目标参数
+			if(r != null) return unescape(r[2]);
+			return null; //返回参数值
+		}
+
 function getCookie(name) {
 	var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
 	if(arr = document.cookie.match(reg)) {
